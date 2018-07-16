474afcd98f76c788172f038b9d473cc6150d0613
Daniel Moniz <daniel.moniz@gmail.com>
2018-07-16 12:12:13 -0400

    Add hello_world and flow_control examples.

--- 06-programming-fundamentals-part-1/instructor/flow_control.rb
+++ 06-programming-fundamentals-part-1/instructor/flow_control.rb
@@ -1,3 +1,39 @@
+
+puts "Please enter a temperature in celsius."
+temperature = gets.chomp.to_i
+# temperature = temperature.to_i
+puts "It is currently #{temperature} in Celsius."
+
+if temperature < 0
+  puts "Really bundle up."
+  # place as much code as you want
+elsif temperature < 14
+  puts "Please bring a jacket."
+elsif temperature < 25
+  puts "Just head on out."
+else
+  puts "Whoa, it's way too hot. Stay in."
+end
+
+
+
+# puts "Hello"
+# puts "Goodbye"
+#
+# thing_should_happen = 5 > 3
+# if thing_should_happen
+#   puts "Yep, this happened!"
+# end
+
+# if something
+#   do stuff
+# end
+#
+# if something else
+#   do stuff
+# end
+
+
 # if statements
 
 # if (some condition is true)
@@ -10,6 +46,16 @@
 
 
 # Looping
+
+purple = 0
+while purple < 15
+  puts "Hello world!"
+  purple += 1
+end
+
+10.times do
+  puts "I will not write on the chalkboard."
+end
 
 # for loop
 # while loop
---
+++ 06-programming-fundamentals-part-1/instructor/hello_world.rb
@@ -0,0 +1,10 @@
+
+puts "Please enter your name."
+
+name = gets.chomp
+
+puts "Your name is #{name}."
+puts name
+puts name.inspect
+
+puts '-------'
