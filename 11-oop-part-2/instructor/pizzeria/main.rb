ee48c12fa3eb84ca908a123e993eb13f7f5a2063
Bitmaker Labs <bitmakerlabs@Bitmaker-3.local>
2018-07-23 11:01:21 -0400

    [11-oop-part-2] Add open status to Pizzeria

--- 11-oop-part-2/instructor/pizzeria/main.rb
+++ 11-oop-part-2/instructor/pizzeria/main.rb
@@ -1,7 +1,12 @@
-require './pizzeria.rb'
+require_relative 'pizzeria'
 
 chez_fred = Pizzeria.new('Fred')
+chez_fred.open
 
 p1 = chez_fred.order(['ham', 'pineapple'])
 
 7.times { p1.eat }
+
+chez_fred.close
+
+p2 = chez_fred.order(['ham', 'pineapple'])
--- 11-oop-part-2/instructor/pizzeria/pizzeria.rb
+++ 11-oop-part-2/instructor/pizzeria/pizzeria.rb
@@ -1,16 +1,30 @@
-require './pizza.rb'
+require_relative 'pizza'
 
 class Pizzeria
 
   def initialize(owner)
     @owner = owner
+
+    @open = false
+  end
+
+  def open
+    @open = true
+  end
+
+  def close
+    @open = false
   end
 
   def order(toppings)
-    pizza = Pizza.new(toppings)
-    pizza.bake
-    3.times { pizza.cut }
-    return pizza
+    if @open
+      pizza = Pizza.new(toppings)
+      pizza.bake
+      3.times { pizza.cut }
+      return pizza
+    else
+      puts 'ERROR: Pizzeria is closed!'
+    end
   end
 
 end
