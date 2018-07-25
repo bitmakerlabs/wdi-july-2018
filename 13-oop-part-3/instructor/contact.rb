6eff6634f8dc4782da1d5c089becb61197612b69
Bitmaker Labs <bitmakerlabs@Bitmaker-3.local>
2018-07-25 12:07:32 -0400

    [13-oop-part-3] Class variables/methods on Contact class

--- 13-oop-part-3/instructor/contact.rb
+++ 13-oop-part-3/instructor/contact.rb
@@ -1,10 +1,32 @@
 class Contact
 
+  @@contacts = []
+  @@next_id = 1000
+
   def initialize(first_name, last_name, email, notes = 'N/A')
     @first_name = first_name
     @last_name = last_name
     @email = email
     @notes = notes
+    @id = nil
+  end
+
+  def self.all
+    @@contacts
+  end
+
+  def self.create(first_name, last_name, email, notes = 'N/A')
+    new_contact = self.new(first_name, last_name, email, notes)
+    new_contact.save
+    return new_contact
+  end
+
+  def self.find(id)
+    # Iterate through the list of contacts (@@contacts)
+    # Find the particular instance with that id
+    # Return that instance
+
+    # Left to the student as homework
   end
 
   # READERS
@@ -45,6 +67,12 @@ class Contact
   def full_name
     # Mixing and Matching, works!
     "#{ @first_name } #{ last_name }"
+  end
+
+  def save
+    @id = @@next_id
+    @@next_id += 1
+    @@contacts << self
   end
 
 end
--- 13-oop-part-3/instructor/main.rb
+++ 13-oop-part-3/instructor/main.rb
@@ -1,11 +1,17 @@
 require_relative 'contact'
 
-clark = Contact.new('Clark', 'Kent', 'clark@dailyplanet.com', 'Goes into the phone booth a lot')
-bruce = Contact.new('Bruce', 'Wayne', 'bruce@wayneenterprises.com', 'Kind of a dark guy')
-diana = Contact.new('Diana', 'Prince', 'diana@amazon.com')
+#clark = Contact.new('Clark', 'Kent', 'clark@dailyplanet.com', 'Goes into the phone booth a lot')
+#bruce = Contact.new('Bruce', 'Wayne', 'bruce@wayneenterprises.com', 'Kind of a dark guy')
+#diana = Contact.new('Diana', 'Prince', 'diana@amazon.com')
+#clark.save
+#bruce.save
+#diana.save
 
-puts diana.full_name
+dave1 = Contact.new('Dave', 'Wong', 'N/A')
+dave2 = Contact.new('Dave', 'Wong', 'N/A')
+dave1.save
+dave2.save
 
-# p clark
-# p bruce
-# p diana
+dave3 = Contact.create('Dave', 'Wong', 'N/A')
+
+person_i_am_looking_for = Contact.find(1001)
