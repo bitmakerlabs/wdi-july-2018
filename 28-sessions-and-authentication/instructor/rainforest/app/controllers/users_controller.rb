ce7353c1893a0e8d8cbc9dd48e4797da878b8699
Bitmaker Labs <bitmakerlabs@Bitmaker-3.local>
2018-08-16 11:56:11 -0400

    [28-sessions-and-authentication] Implement UsersController#create, display error messages on fields with errors

--- 28-sessions-and-authentication/instructor/rainforest/app/controllers/users_controller.rb
+++ 28-sessions-and-authentication/instructor/rainforest/app/controllers/users_controller.rb
@@ -4,5 +4,20 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new
+    @user.email = params[:user][:email]
+    @user.password = params[:user][:password]
+    @user.password_confirmation = params[:user][:password_confirmation]
+
+    if @user.save
+      # flash: 'Welcome to Rainforest!'
+
+      # In a view, always use _path
+      # In a controller, always use _url
+      redirect_to root_url
+    else
+      # flash: 'Error, please try again!'
+      render :new
+    end
   end
 end
--- 28-sessions-and-authentication/instructor/rainforest/config/routes.rb
+++ 28-sessions-and-authentication/instructor/rainforest/config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-
   resources :products, only: %i(index)
   resource  :users, only: %i(new create)
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'products#index'
 end
