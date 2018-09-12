d658d348e8d8320f1f01eb50b206a3cb0efba6c0
Gurjant Singh Sadhra <gurjant31@gmail.com>
2018-09-12 11:49:39 -0400

    Lecture 41 - instructor

--- 41-ajax-and-json/instructor/robots/app/assets/javascripts/robots.js
+++ 41-ajax-and-json/instructor/robots/app/assets/javascripts/robots.js
@@ -3,4 +3,62 @@
 
 document.addEventListener('DOMContentLoaded', function() {
 
+//get all links
+var robocont = document.querySelector('#robot-container');
+var robodetails = document.querySelector('#robot-details');
+// console.log(robolinks);
+
+
+// for (var i =0; i < robolinks.length;i++) {
+
+// 	robolinks[i].addEventListener('click',function(e){
+// 		e.preventDefault();
+		
+// 		console.log(e.target.parentNode.href)
+
+// 	}) }
+
+
+robocont.addEventListener('click',function(e){
+
+	e.preventDefault();
+
+	var robotlink = e.target.parentNode.href
+
+	if (robotlink){
+
+		// console.log(e.target.parentNode.href)
+		$.ajax({
+			url: robotlink,
+			method: 'GET',
+			dataType: 'json'
+		}).done(function(data){
+			// console.log(data);
+			// robodetails.innerHTML = data;
+
+			// var divTag = document.createElement('div');
+			// var imgTag = document.createElement('img');
+			// imgTag.src = 'http://robohash.org/'+ data.address;
+			// var pTag = document.createElement('p');
+			// pTag.innerText = data.name
+			// divTag.append(imgTag)
+			// divTag.append(pTag)
+			// robodetails.innerHTML = divTag.innerHTML;
+
+			// robodetails.innerHTML = `<img src="http://robohash.org/`+ data.address+`"/><p>`+ data.name+ `</p>`
+
+			var source   = document.getElementById("entry-template").innerHTML;
+			var template = Handlebars.compile(source);	
+			robodetails.innerHTML = template(data);
+
+		});
+
+	}
+
+});
+
+		
+
+
+
 });
